3a623a0eed90d863ea2e8881d97178b12de71b83
₦ł₵₭  ₣ɄⱤɎ <96591657+NICK-FURY-6023@users.noreply.github.com>
2024-07-20 18:36:08 +0530

    Update shardError.ts

--- src/events/shard/shardError.ts
+++ src/events/shard/shardError.ts
@@ -1,1 +1,7 @@
+import { Manager } from "../../manager.js";
 
+export default class {
+  async execute(client: Manager, id: number) {
+    client.logger.error(import.meta.url, `Shard ${id} Errored!`);
+  }
+}
