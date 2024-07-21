30ea57272884d8d404294154b0f7978e58a325a0
₦ł₵₭  ₣ɄⱤɎ <96591657+NICK-FURY-6023@users.noreply.github.com>
2024-07-21 18:48:32 +0530

    Update nodeDisconnect.ts

--- src/events/node/nodeDisconnect.ts
+++ src/events/node/nodeDisconnect.ts
@@ -1,1 +1,14 @@
+import { Manager } from "../../manager.js";
+import { RainlinkNode } from "../../rainlink/main.js";
 
+export default class {
+  execute(client: Manager, node: RainlinkNode, code: number, reason: Buffer) {
+    client.rainlink.players.forEach((player, index) => {
+      if (player.node.options.name == node.options.name) player.destroy();
+    });
+    client.logger.debug(
+      import.meta.url,
+      `Lavalink ${node.options.name}: Disconnected, Code: ${code}, Reason: ${reason}`
+    );
+  }
+}
