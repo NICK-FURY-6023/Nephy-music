ea2f96eeb30586f817fe01fa45316a1661cffc2d
₦ł₵₭  ₣ɄⱤɎ <96591657+NICK-FURY-6023@users.noreply.github.com>
2024-07-23 19:09:27 +0530

    Update warn.ts

--- src/events/client/warn.ts
+++ src/events/client/warn.ts
@@ -1,1 +1,7 @@
+import { Manager } from "../../manager.js";
 
+export default class {
+  async execute(client: Manager) {
+    client.logger.warn(import.meta.url, `Warned ${client.user!.tag} (${client.user!.id})`);
+  }
+}
