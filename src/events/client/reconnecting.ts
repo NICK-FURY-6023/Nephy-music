d3f29b004a63ee54da41f10eb609a0b78083324a
₦ł₵₭  ₣ɄⱤɎ <96591657+NICK-FURY-6023@users.noreply.github.com>
2024-07-23 19:13:01 +0530

    Update reconnecting.ts

--- src/events/client/reconnecting.ts
+++ src/events/client/reconnecting.ts
@@ -1,1 +1,7 @@
+import { Manager } from "../../manager.js";
 
+export default class {
+  async execute(client: Manager) {
+    client.logger.info(import.meta.url, `Reconnected ${client.user!.tag} (${client.user!.id})`);
+  }
+}
