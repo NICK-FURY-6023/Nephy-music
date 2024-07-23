0e0a8750126c013de1c39cad71afaf2d5c1cbae0
₦ł₵₭  ₣ɄⱤɎ <96591657+NICK-FURY-6023@users.noreply.github.com>
2024-07-23 19:11:39 +0530

    Update rateLimit.ts

--- src/events/client/rateLimit.ts
+++ src/events/client/rateLimit.ts
@@ -1,1 +1,7 @@
+import { Manager } from "../../manager.js";
 
+export default class {
+  async execute(client: Manager) {
+    client.logger.error(import.meta.url, `Rate Limited, Sleeping for ${0} seconds`);
+  }
+}
