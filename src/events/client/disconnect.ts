891307a27a502fa2669221b5c1454af10add8cdc
₦ł₵₭  ₣ɄⱤɎ <96591657+NICK-FURY-6023@users.noreply.github.com>
2024-07-23 19:12:26 +0530

    Update disconnect.ts

--- src/events/client/disconnect.ts
+++ src/events/client/disconnect.ts
@@ -1,1 +1,7 @@
+import { Manager } from "../../manager.js";
 
+export default class {
+  async execute(client: Manager) {
+    client.logger.info(import.meta.url, `Disconnected ${client.user!.tag} (${client.user!.id})`);
+  }
+}
