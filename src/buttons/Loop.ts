accef7d12183cd73bdbbb54d3d9f266fe0351580
₦ł₵₭  ₣ɄⱤɎ <partham302@gmail.com>
2024-08-15 12:13:38 +0000

    fix  loop

--- src/buttons/Loop.ts
+++ src/buttons/Loop.ts
@@ -1,75 +1,86 @@
-import { ButtonInteraction, CacheType, InteractionCollector, Message } from "discord.js";
-import { PlayerButton } from "../@types/Button.js";
-import { Manager } from "../manager.js";
-import { ReplyInteractionService } from "../services/ReplyInteractionService.js";
-import { RainlinkLoopMode, RainlinkPlayer } from "../rainlink/main.js";
+import { ButtonInteraction, CacheType, InteractionCollector, Message } from 'discord.js'
+import { PlayerButton } from '../@types/Button.js'
+import { Manager } from '../manager.js'
+import { ReplyInteractionService } from '../services/ReplyInteractionService.js'
+import { RainlinkLoopMode, RainlinkPlayer } from '../rainlink/main.js'
 
 export default class implements PlayerButton {
-  name = "loop";
+  name = 'loop'
   async run(
     client: Manager,
     message: ButtonInteraction<CacheType>,
     language: string,
     player: RainlinkPlayer,
     nplaying: Message<boolean>,
-    collector: InteractionCollector<ButtonInteraction<"cached">>
+    collector: InteractionCollector<ButtonInteraction<'cached'>>
   ): Promise<any> {
     if (!player) {
-      collector.stop();
+      collector.stop()
     }
 
     async function setLoop247(loop: string) {
       if (await client.db.autoreconnect.get(player.guildId)) {
-        await client.db.autoreconnect.set(`${player.guildId}.config.loop`, loop);
+        await client.db.autoreconnect.set(`${player.guildId}.config.loop`, loop)
       }
     }
 
     switch (player.loop) {
-      case "none":
-        await player.setLoop(RainlinkLoopMode.SONG);
+      case 'none':
+        player.setLoop(RainlinkLoopMode.SONG)
 
-        setLoop247(RainlinkLoopMode.SONG);
+        if (client.config.utilities.AUTO_RESUME) setLoop247(RainlinkLoopMode.SONG)
 
-        new ReplyInteractionService(client, message, `${client.getString(language, "button.music", "loop_current")}`);
-
-        client.wsl.get(message.guild!.id)?.send({
-          op: "playerLoop",
-          guild: message.guild!.id,
-          mode: "song",
-        });
-
-        break;
-
-      case "song":
-        player.setLoop(RainlinkLoopMode.QUEUE);
-
-        setLoop247(RainlinkLoopMode.QUEUE);
-
-        new ReplyInteractionService(client, message, `${client.getString(language, "button.music", "loop_all")}`);
+        new ReplyInteractionService(
+          client,
+          message,
+          `${client.i18n.get(language, 'button.music', 'loop_current')}`
+        )
 
         client.wsl.get(message.guild!.id)?.send({
-          op: "playerLoop",
+          op: 'playerLoop',
           guild: message.guild!.id,
-          mode: "queue",
-        });
+          mode: 'song',
+        })
 
-        break;
+        break
 
-      case "queue":
-        await player.setLoop(RainlinkLoopMode.NONE);
+      case 'song':
+        player.setLoop(RainlinkLoopMode.QUEUE)
 
-        setLoop247(RainlinkLoopMode.NONE);
+        if (client.config.utilities.AUTO_RESUME) setLoop247(RainlinkLoopMode.QUEUE)
 
-        new ReplyInteractionService(client, message, `${client.getString(language, "button.music", "unloop_all")}`);
+        new ReplyInteractionService(
+          client,
+          message,
+          `${client.i18n.get(language, 'button.music', 'loop_all')}`
+        )
 
         client.wsl.get(message.guild!.id)?.send({
-          op: "playerLoop",
+          op: 'playerLoop',
           guild: message.guild!.id,
-          mode: "none",
-        });
+          mode: 'queue',
+        })
 
-        break;
+        break
+
+      case 'queue':
+        player.setLoop(RainlinkLoopMode.NONE)
+
+        if (client.config.utilities.AUTO_RESUME) setLoop247(RainlinkLoopMode.NONE)
+
+        new ReplyInteractionService(
+          client,
+          message,
+          `${client.i18n.get(language, 'button.music', 'unloop_all')}`
+        )
+
+        client.wsl.get(message.guild!.id)?.send({
+          op: 'playerLoop',
+          guild: message.guild!.id,
+          mode: 'none',
+        })
+
+        break
     }
   }
 }
-
