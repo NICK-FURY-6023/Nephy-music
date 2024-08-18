669d6dabc6b090e1cd037383a2266f655e2bf780
₦ł₵₭  ₣ɄⱤɎ <partham302@gmail.com>
2024-08-18 13:13:02 +0000

    fix pause

--- src/buttons/Pause.ts
+++ src/buttons/Pause.ts
@@ -1,38 +1,47 @@
-import { ButtonInteraction, CacheType, InteractionCollector, Message } from "discord.js";
-import { PlayerButton } from "../@types/Button.js";
-import { Manager } from "../manager.js";
-import { playerRowOne, playerRowOneEdited, playerRowTwo } from "../assets/PlayerControlButton.js";
-import { ReplyInteractionService } from "../services/ReplyInteractionService.js";
-import { RainlinkPlayer } from "../rainlink/main.js";
+import { ButtonInteraction, CacheType, InteractionCollector, Message } from 'discord.js'
+import { PlayerButton } from '../@types/Button.js'
+import { Manager } from '../manager.js'
+import {
+  filterSelect,
+  playerRowOne,
+  playerRowOneEdited,
+  playerRowTwo,
+} from '../utilities/PlayerControlButton.js'
+import { ReplyInteractionService } from '../services/ReplyInteractionService.js'
+import { RainlinkPlayer } from '../rainlink/main.js'
 
 export default class implements PlayerButton {
-  name = "pause";
+  name = 'pause'
   async run(
     client: Manager,
     message: ButtonInteraction<CacheType>,
     language: string,
     player: RainlinkPlayer,
     nplaying: Message<boolean>,
-    collector: InteractionCollector<ButtonInteraction<"cached">>
+    collector: InteractionCollector<ButtonInteraction<'cached'>>
   ): Promise<any> {
     if (!player) {
-      collector.stop();
+      collector.stop()
     }
 
-    const newPlayer = await player.setPause(!player.paused);
+    const newPlayer = await player.setPause(!player.paused)
 
     newPlayer.paused
-      ? nplaying.edit({
-          components: [playerRowOneEdited, playerRowTwo],
-        })
-      : nplaying.edit({
-          components: [playerRowOne, playerRowTwo],
-        });
+      ? nplaying
+          .edit({
+            components: [playerRowOneEdited(client), playerRowTwo(client), filterSelect(client)],
+          })
+          .catch(() => null)
+      : nplaying
+          .edit({
+            components: [playerRowOne(client), playerRowTwo(client), filterSelect(client)],
+          })
+          .catch(() => null)
 
     new ReplyInteractionService(
       client,
       message,
-      `${client.getString(language, "button.music", newPlayer.paused ? "pause_msg" : "resume_msg")}`
-    );
+      `${client.i18n.get(language, 'button.music', newPlayer.paused ? 'pause_msg' : 'resume_msg')}`
+    )
   }
 }
